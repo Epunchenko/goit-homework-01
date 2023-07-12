f26dfacbc5b30600bfcbfd473800491d0d337237
Epunchenko <107404865+Epunchenko@users.noreply.github.com>
2023-07-12 07:11:31 +0300

    Add files via upload

---
+++ modul_1_1.py
@@ -0,0 +1,35 @@
+"""
+#5
+PI = 3.14
+radius_r = float(input("Enter Radius: "))
+
+circle = round(2*PI*radius_r, 2)
+area = (PI*radius_r**2)/2
+
+print(f"Circle length: {circle} \nArea of circle: {area}")
+
+
+print("Resalt of tree real number: ", abs(float(input("First number: ")))
+                                    + abs(float(input("second number: ")))
+                                    + abs(float(input("Third number: "))))
+"""
+
+"""
+#6
+x = abs(float(input("Enter your number: ")))
+
+expression = int(2*x*4-3*x*3+4*x*2-5*x+6)
+print(expression)
+"""
+"""
+#7
+x1 = float(input("Enter x1: "))
+x2 = float(input("Enter x2: "))
+y1 = float(input("Enter y1: "))
+y2 = float(input("Enter y2: "))
+distance = round(((x2-x1)**2 + (y2 - y1)**2)**0.5, 2)
+print(f"Distance: {distance}")
+print("Distnce is equal: ", distance)
+"""
+
+#8
